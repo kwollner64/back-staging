0e69ddf68da40899b809cba9c3c68396f5d8dac7
thisonewillworkforsure <ben6228@yahoo.com>
2022-10-10 22:10:30 -0400

    IS BACKEND GOING TO WORK?

---
+++ social-media/src/main/java/com/revature/controllers/ProfileController.java
@@ -0,0 +1,61 @@
+package com.revature.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.annotations.Authorized;
+import com.revature.models.Profile;
+import com.revature.models.User;
+import com.revature.services.ProfileService;
+
+@RestController
+@RequestMapping("/profiles")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+public class ProfileController {
+
+	@Autowired
+	private ProfileService profileService;
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Profile>>getAllProfile(){
+		
+		return ResponseEntity.ok(profileService.getAllProfile());
+	}
+	
+	@PostMapping("/page")
+	public ResponseEntity<Profile>getOneProfile(@RequestBody User user){
+		
+		return ResponseEntity.ok(profileService.getProfile(user));
+	}
+	
+	@PostMapping("/{pid}")
+	public ResponseEntity<Profile>getOneProfile(@PathVariable("pid")int id){
+		
+		User user = new User(id,"","","","");
+		return ResponseEntity.ok(profileService.getProfile(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Profile>createProfile(@RequestBody Profile profile){
+		
+		return ResponseEntity.ok(profileService.createProfile(profile));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Profile>updateProfile(@RequestBody Profile profile){
+		
+		return ResponseEntity.ok(profileService.updateProfile(profile));
+	}
+	
+}
---
+++ social-media/src/test/java/com/revature/ProfileServiceTest.java
@@ -0,0 +1,91 @@
+package com.revature;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.revature.models.Profile;
+import com.revature.models.User;
+import com.revature.repositories.ProfileRepository;
+import com.revature.services.ProfileService;
+
+@ExtendWith(MockitoExtension.class)
+public class ProfileServiceTest {
+
+	@InjectMocks
+	private ProfileService profileService;
+	
+	@Mock
+	private Profile profile;
+	
+	@Mock
+	private Profile expectedProfile;
+	
+	@Mock
+	private User user;
+	
+	@Mock
+	private ProfileRepository profileRepository;
+	
+	@Mock
+	private List<Profile> profiles;
+	
+	@BeforeEach
+	public void beforeEach() {
+		user = new User(1,"testuser@gmail.com","password","test","user");
+		profile = new Profile(-1,"text from profile","img_url",user);
+		expectedProfile = new Profile(1,"text from expectedProfile","img_url",user);
+		profiles = new ArrayList<Profile>();
+	}
+	
+	@Test
+	public void testUpdateProfile() {
+		Mockito.when(profileRepository.save(profile)).thenReturn(expectedProfile);
+		profile = profileService.updateProfile(profile);
+		assertEquals(expectedProfile.getText(), profile.getText());
+	}
+	
+	@Test
+	public void testCreateProfile() {
+		Mockito.when(profileRepository.saveAndFlush(profile)).thenReturn(expectedProfile);
+		profile = profileService.createProfile(profile);
+		assertEquals(expectedProfile.getText(), profile.getText());
+	}
+	
+	@Test
+	public void testGetAllProfile() {
+		List<Profile> expectedProfiles = new ArrayList<Profile>();
+		expectedProfiles.add(profile);
+		expectedProfiles.add(expectedProfile);
+		Mockito.when(profileRepository.findAll()).thenReturn(expectedProfiles);
+		profiles = profileService.getAllProfile();
+		assertEquals(expectedProfiles.size(),profiles.size());
+	}
+	
+	@Test
+	public void testGetOneProfile() {
+		profiles.add(expectedProfile);
+		Mockito.when(profileRepository.findByUser(user)).thenReturn(profiles);
+		profile = profileService.getProfile(user);
+		assertEquals(profiles.get(0).getId(), profile.getId());
+	}
+	
+	//This test is to check for when a profile is not found
+	@Test
+	public void testGetOneProfileNotFound() {
+		profile.setId(-10);
+		Mockito.when(profileRepository.findByUser(user)).thenReturn(profiles);
+		profile = profileService.getProfile(user);
+		assertEquals(-1, profile.getId());
+	}
+}
