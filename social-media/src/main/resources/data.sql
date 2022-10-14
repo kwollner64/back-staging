0c5850245f46329cb79bf29ff828c97321624c65
HaruYus <yusufharuon@gmail.com>
2022-10-14 12:39:43 -0500

    Added bookmark repo, serv and controller

--- social-media/src/main/resources/data.sql
+++ social-media/src/main/resources/data.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 --
 --INSERT INTO users (id, email, password, first_name, last_name) VALUES (
 --    1,
@@ -8,8 +7,6 @@
 --    'User'
 --);
 
-=======
->>>>>>> origin/develop
 INSERT INTO users (email, password, first_name, last_name) VALUES (
     'testuser@gmail.com',
     'password',
