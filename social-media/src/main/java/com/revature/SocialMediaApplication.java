5ad779b62621856cf2e4047e4ac438d958a2b743
thisonewillworkforsure <ben6228@yahoo.com>
2022-10-15 22:08:28 -0400

    can upload stuff to uploads folder

--- social-media/src/main/java/com/revature/SocialMediaApplication.java
+++ social-media/src/main/java/com/revature/SocialMediaApplication.java
@@ -1,13 +1,26 @@
 package com.revature;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SocialMediaApplication {
+import com.revature.services.FileStorageService;
 
+@SpringBootApplication
+public class SocialMediaApplication implements CommandLineRunner {
+
+	@Resource
+	FileStorageService storageService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SocialMediaApplication.class, args);
 	}
 
+	@Override
+	  public void run(String... arg) throws Exception {
+	    storageService.deleteAll();
+	    storageService.init();
+	  }
 }
