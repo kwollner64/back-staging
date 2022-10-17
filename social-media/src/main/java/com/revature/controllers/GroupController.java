684ca0f3e9a46c85c58eb92f2e4274c7c753a22a
kwollner64 <110429380+kwollner64@users.noreply.github.com>
2022-10-17 15:45:11 -0500

    merge

---
+++ social-media/src/main/java/com/revature/controllers/GroupController.java
@@ -0,0 +1,73 @@
+package com.revature.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.annotations.Authorized;
+import com.revature.models.Group;
+import com.revature.models.GroupPost;
+import com.revature.models.Post;
+import com.revature.models.User;
+import com.revature.services.GroupService;
+
+@RestController
+@RequestMapping("/groups")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+public class GroupController {
+	
+	@Autowired
+	private GroupService groupService;
+	
+	//@Authorized
+	@GetMapping("")
+	public ResponseEntity<List<Group>> getAllGroups(){
+		return ResponseEntity.ok(this.groupService.getAllGroups());
+	}
+	
+	// GET A GROUP
+	//@Authorized
+	@GetMapping("/group/{groupID}")
+	public ResponseEntity<Optional<Group>> getGroup(@PathVariable("groupID") Integer groupID){
+		return ResponseEntity.ok(this.groupService.getGroup(groupID));
+	}
+	
+	// CREATE GROUP
+	// @Authorized
+	@PostMapping("")
+	public void createGroup(@RequestBody Group newGroup) {
+		this.groupService.createGroup(newGroup);
+	}
+	
+	// ADD GROUP MEMBER
+	//@Authorized
+	@PostMapping("/member/{groupID}")
+	public void addMember(@PathVariable("groupID") Integer groupID, @RequestBody User user) {
+		groupService.addMember(user, groupID);
+	}
+	
+	// DELETE GROUP MEMBER
+	@DeleteMapping("")
+	public void deleteMember(int userID, int groupID) {
+		this.groupService.deleteMember(userID, groupID);
+	}
+	
+	// EDIT GROUP
+	@PutMapping("")
+	public ResponseEntity<Group> editGroup(@RequestBody Group updatedGroup){
+		return ResponseEntity.ok(groupService.editGroup(updatedGroup));
+	}
+	
+	
+}
---
+++ social-media/src/main/java/com/revature/controllers/GroupPostController.java
@@ -0,0 +1,42 @@
+package com.revature.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.Group;
+import com.revature.models.GroupPost;
+import com.revature.services.GroupPostService;
+
+@RestController
+@RequestMapping("/group-posts")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+public class GroupPostController {
+	
+	@Autowired
+	GroupPostService groupPostService;
+	
+	@GetMapping
+	public ResponseEntity<Optional<List<GroupPost>>> getAllPosts(Integer groupID){
+		return ResponseEntity.ok(this.groupPostService.getAllPosts(groupID));
+	}
+
+	// NEW POST
+	//@Authorized
+	@PostMapping("")
+	public ResponseEntity<GroupPost> upsertPost(@RequestBody GroupPost post){
+		return ResponseEntity.ok(this.groupPostService.upsert(post));
+	}
+	
+	
+}
